827c68ad8900f2ca9a31e7be9290657c4b8921c8
LeeEugene1 <uwm1004@gmail.com>
2026-01-13 20:14:46 +0900

    feat: 함수, 클래스 컴포넌트

--- src/app.js
+++ src/app.js
@@ -1,48 +1,23 @@
-import { render } from './react';
+/* @jsx createElement */
+import { render, createElement, Component } from './react';//제약사항: 런타임에러때문에 항상 리엑트를 임포트하듯이 createElement를 사용해야함
 
-const vdom = {
-    tag:'p',
-    props:{},
-    children:[
-        {
-            tag:'h1',
-            props:{},
-            children:["react 만들기"],
-        },
-        {
-            tag:'ul',
-            props:{},
-            children:[
-                {
-                    tag:'li',
-                    props:{
-                        style: "color:red",
-                    },
-                    children:[
-                        '첫번쨰'
-                    ]
-                },
-                {
-                    tag:'li',
-                    props:{
-                        style: "color:blue",
-                    },
-                    children:[
-                        '두번쨰'
-                    ]
-                },
-                {
-                    tag:'li',
-                    props:{
-                        style: "color:green",
-                    },
-                    children:[
-                        '세번쨰'
-                    ]
-                }
-            ]
-        }
-    ]
+//클래스 컴포넌트
+class Title extends Component{
+    render(){
+        return <h1>{this.props.children}</h1>
+    }
 }
+//함수 컴포넌트
+function Item(props){
+    return <li style={`color:${props.color}`}>{props.children}</li>
+}
+const vdom = <p>
+    <Title>React</Title>
+    <ul>
+        <Item color="red">첫번째</Item>
+        <Item color="blue">두번째</Item>
+        <Item color="green">세번째</Item>
+    </ul>
+</p>
 
 render(vdom, document.querySelector('#root'));
--- src/react.js
+++ src/react.js
@@ -1,3 +1,9 @@
+export class Component{
+    constructor(props){
+        this.props = props;
+    }
+}
+
 export function createDom(node){
     if(typeof node === 'string'){
         return document.createTextNode(node);
@@ -15,4 +21,34 @@ export function createDom(node){
 
 export function render(vdom, container){
 	container.appendChild(createDom(vdom))
+}
+
+function makeProps(props, children){
+    return {
+        ...props,
+        children: children.length === 1 ? children[0] : children
+    }
+}
+
+export function createElement(tag, props, ...children){
+    props = props || {};//@jsx가 빈객체를 null로 변환해 에러 발생하므로 빈객체로 변환
+	
+    if(typeof tag === 'function'){
+        //클래스 컴포넌트
+        if(tag.prototype instanceof Component){
+            const instance = new tag(makeProps(props, children));
+            return instance.render();
+        }else{
+            //함수 컴포넌트
+            if(children.length > 0){
+                return tag(makeProps(props, children));
+            }else{
+                return tag(props);
+            }
+        }
+        
+    }else{
+        return { tag, props, children }
+    }
+    
 }
